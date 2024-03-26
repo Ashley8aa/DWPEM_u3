a48bc28f1a3f025f2def4bff3d35b49411c58029
Ashley8aa <a1118120096@utch.edu.mx>
2024-03-26 09:09:41 -0600

    Mejoras en el formulario

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductsService } from '../productservice/products.service';
-import { ProductForm } from '../products';
-import { Router , RouterLink} from '@angular/router';
+import { Products } from '../products';
+import { ActivatedRoute, Router , RouterLink} from '@angular/router';
 
 export interface CreateForm {
   name: FormControl<string>;
@@ -22,46 +22,45 @@ export class ProductFormComponent {
   private _formBuilder = inject(FormBuilder).nonNullable;
   private _router = inject(Router);
   private _productId = '';
+  private _product: Products | undefined
 
   get productId(): string {
     return this._productId;
   }
 
-  
   @Input() set productId(value: string){
     this._productId = value;
-    this.setFormValues(this._productId)
   }
-
 
   productForm!: FormGroup;
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb:FormBuilder,private route: ActivatedRoute) {
+    this._productId = this.route.snapshot.paramMap.get('id')!;
+    this._productsService.getProduct(this._productId).then((product) => {
+      this._product = product!;
+      this.setFormValues(this._productId)
+    });
+    
     this.productForm = this.fb.group({
       name: new FormControl("", [Validators.required]),
       price: new FormControl("", [Validators.required]),
       description: new FormControl("", [Validators.required])  
     })
   }
-
-
-  onSubmit(){
-    if(this.productForm.valid){    
-      const formData = this.productForm.value;
-      this._productsService.createProducts(formData).then(() => {
-        console.log('Product added successfully.');
-        // You can optionally reset the form after successful submission
-        this._router.navigate(['/store']);
-      }).catch(error => {
-        console.error('Error adding product: ', error);
-      });
-    } else {
-      this.productForm.markAllAsTouched();
+//Todo esto se encarga del form (agregar y editar producto)
+  async onSubmit(){
+    if (this.productForm.invalid) return;  //Si el prducto tiene id se ira al metodo de updateProducts
+    try {
+      const product = this.productForm.value as Products;
+      !this._product
+        ? await this._productsService.createProducts(product)
+        : await this._productsService.updateProduct(this.productId, product);
+      this._router.navigate(['/store']);
+    } catch (error) {
+      //
     }
   }
-
-
-
+//Esto se usa para popular el formulario si es que se le da clic a editar
   async setFormValues(id: string) {
     try {
       const product = await this._productsService.getProduct(id);
@@ -71,13 +70,8 @@ export class ProductFormComponent {
         price: product.price,
         description: product.description,
       });
-
     } catch (error) {}
   }
-
- 
-
-
 }
 
 
