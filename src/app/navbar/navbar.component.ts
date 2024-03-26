38ad912ac3a21bf3b9aebc591a27bf4dbbe68f00
Ashley8aa <a1118120096@utch.edu.mx>
2024-03-26 16:43:42 -0600

    Mejoras auth

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LandingComponent } from '../landing/landing.component';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LandingComponent],
+  imports: [LandingComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(
+    private router: Router // Inject the Router service
+  ) {}
+  authService = inject(AuthService);
 
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/landing']);
+  }
 }
