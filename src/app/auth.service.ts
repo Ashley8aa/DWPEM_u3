fa85f3750e33a0d8f81ae930197ebf471f6dcdd9
Ashley8aa <a1118120096@utch.edu.mx>
2024-03-26 11:31:02 -0600

    Mejoras auth

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,19 @@
-import { Injectable, inject } from "@angular/core";
-import { Auth, signInWithEmailAndPassword } from "@angular/fire/auth";
+import { Injectable, inject, signal } from "@angular/core";
+import { Auth, signInWithEmailAndPassword, user } from "@angular/fire/auth";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { Observable, from } from "rxjs";
+import { UserInterface } from "./user.interface";
 
 @Injectable({
     providedIn: 'root'
 
 })
 export class AuthService {
-    firebaseAuth = inject(Auth)
+    firebaseAuth = inject(Auth); //esto permite utilizar metodos de firebase
+    //Para la evidencia 2 u3, esto ayuda manejar cambios en el estado de autenticación del usuario.
+    user$ = user(this.firebaseAuth);
+    currentUserSig = signal<UserInterface | null | undefined>(undefined);
+    
 
     register(email: string, username: string, password: string): Observable<void> {
         const promise = createUserWithEmailAndPassword(this.firebaseAuth, email, 
@@ -27,7 +32,6 @@ export class AuthService {
             password,
         ).then(() => {});
         return from(promise);
-
     }
 }
 
