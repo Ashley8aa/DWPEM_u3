f9e769904dddbd68ef11315dce3f66b43578beea
Ashley8aa <a1118120096@utch.edu.mx>
2024-03-26 09:39:05 -0600

    Mejoras al codigo

--- src/app/productservice/products.service.ts
+++ src/app/productservice/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, addDoc, collectionData, updateDoc, doc, DocumentReference, getDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData, updateDoc, doc, DocumentReference, getDoc, deleteDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { ProductForm, Products } from '../products';
 
@@ -13,11 +13,12 @@ export class ProductsService {
   private _firestore = inject(Firestore);
   private _collection = collection(this._firestore, PATH);
 
+  //Esta función recupera todos los productos de la colección... Devuelve un Observable de una serie de Productos.
   getProducts() {
     return collectionData(this._collection, {idField: 'id'}) as Observable<
     Products[]>;
   }
-
+ //Esta función recupera un único producto de la colección en función de su id. Devuelve una Promesa que se resuelve en un único objeto Productos. 
   async getProduct(id: string){
     try {
       const document = doc(this._firestore, PATH, id); //recibir referencia a firestore
@@ -28,6 +29,7 @@ export class ProductsService {
     }
   }
 
+  //Para CRUD
   createProducts(product: ProductForm){
     return addDoc(this._collection, product);
   }
@@ -37,5 +39,10 @@ export class ProductsService {
     return updateDoc(document, {...product}); //spread operator, crea un nuevo objeto con las mismas propiedades
   }
 
+  deleteProduct(id: string) {
+    const document = doc(this._firestore, PATH, id); //recibir referencia a firestore
+    return deleteDoc(document);
+  }
 }
+
 
